ab299c91c09726cf70a40987ea6ddc3934e99f83
OlgaSomova <126794249+OlgaSomova@users.noreply.github.com>
2024-05-08 16:59:12 +0200

    Update test calculation

--- tests/test_calculation.py
+++ tests/test_calculation.py
@@ -1,49 +1,40 @@
 """Test calculation functions."""
 
-try:
-    from vctlib.calculation import (
-        run_vct_simulation,
-        get_climate_data_from_epw,
-        get_climate_data_from_csv,
-        get_vent_mode_over_year,
-        get_requirend_frequency_air_change_rate,
-        get_annual_data,
-    )
-    from vctlib.model import Building, ClimateData, ThermostaticalProperties
-except ModuleNotFoundError:  # TODO: remove try-except
-    import sys
-
-    sys.path.insert(1, "/home/osomova/Projects/vct/vctlib/src")
-    from vctlib.model import Building, ClimateData, ThermostaticalProperties
-    from vctlib.calculation import (
-        run_vct_simulation,
-        get_climate_data_from_epw,
-        get_climate_data_from_csv,
-        get_vent_mode_over_year,
-        get_requirend_frequency_air_change_rate,
-        get_annual_data,
-    )
-
-
+import numpy as np
 import pandas as pd
+
+from vctlib.calculation import (
+    run_vct_simulation,
+    get_climate_data_from_epw,
+    get_climate_data_from_csv,
+    get_vent_mode_over_year,
+    get_requirend_frequency_air_change_rate,
+    get_annual_data,
+)
+from inputs.functions import (
+    get_appartment_bld_climate_data, 
+    get_appartment_bld_thermophys_props, 
+    get_appartment_building, 
+    load_data_from_VCdesign
+)
+
 
 __author__ = "OlgaSomova"
 __copyright__ = "OlgaSomova"
 __license__ = "MIT"
 
 
-def get_original_test_climate_data():
-    outdoor_dry_bulb_temperature = pd.read_csv("tests/data/outdor_dry_bulb_temperature.csv")
-    relative_humidity_outdoor_air = pd.read_csv("tests/data/outdor_climate_data.csv")
-    isol_tot = pd.read_csv("tests/data/isol_tot.csv")
-    isol_tot = isol_tot.iloc[:, 0].values
+ERROR = 10**-8  # Acceptable error = 1e-8
 
-    climate_data = ClimateData(
-        outdoor_dry_bulb_temperature=outdoor_dry_bulb_temperature,
-        relative_humidity_outdoor_air=relative_humidity_outdoor_air,
-        isol_tot=isol_tot
-    )
-    return climate_data
+
+def compare_dataframes(df1: pd.DataFrame, df2: pd.DataFrame):
+    """ Compare two dataframes with tolerance 1e-8 """
+    arr1 = df1.values.astype(float)
+    arr2 = df2.values.astype(float)
+
+    are_equal = np.allclose(np.sort(arr1.flatten()), np.sort(arr2.flatten()), atol=ERROR, equal_nan=True)
+    return are_equal
+
 
 def test_get_climate_data_from_epw(snapshot):
     filename = "src/vctlib/temp_data/ITA_Bolzano.160200_IGDG.epw"
@@ -63,50 +54,21 @@ def test_get_climate_data_from_csv(snapshot):
 
 def test_snapshot_building_simulation(snapshot):
     """Test Appartment building."""
-    thermophys_prop = ThermostaticalProperties(
-        external_wall_area=9.6 + 16.2 + 16.2 + 21.6,
-        floor_area=6 * 8,
-        roof_area=6 * 8,
-        external_wall_r=1.797,
-        floor_r=25.246,
-        roof_r=2.992,
-    )
-    inputs = Building(
-        bui_type="Apartment building",
-        celing_to_floor_height=2.7,
-        envelope_area=171.60,
-        floor_area=48.00,
-        fenestration_area=12.00,
-        comfort_requirements="category II",
-        max_outdoor_rel_hum_accepted=85,
-        u_value_opaque=thermophys_prop.u_value_tot,
-        u_value_fen=2.984,
-        construction_mass="medium",
-        g_value_glazing_sys=0.71,
-        shading_control_setpoint=120,
-        shading_factor=0,
-        vent_rates_mu="1/h",
-        time_control_on=0,
-        time_control_off=24,
-        ti_hsp_day_start=7,
-        ti_hsp_night_start=24,
-    )
-    climate_data = get_original_test_climate_data()
+    thermophys_prop = get_appartment_bld_thermophys_props()
+    inputs = get_appartment_building(thermophys_prop)
+    climate_data = get_appartment_bld_climate_data()
 
     df = run_vct_simulation(inputs, climate_data, thermophys_prop)
 
-    df_original = pd.read_csv("tests/data/apartment_building.csv")
+    df_original = pd.read_csv("tests/inputs/data/apartment_building.csv")
     df = df.drop(columns=["Date", "Time", "Day"])
     df_original = df_original.drop(columns=["Date", "Time", "Day"])
 
-    # Acceptable error = 1e-9
-    error = 10**-9
-
     result = {}
     for col in df.columns:
-        diff = [abs(x - y) for x, y in zip(df[col].values, df_original[col].values)]
+        # diff = [abs(x - y) for x, y in zip(df[col].values, df_original[col].values)]
         diff_array = [
-            abs(x - y) < error for x, y in zip(df[col].values, df_original[col].values)
+            abs(x - y) < ERROR for x, y in zip(df[col].values, df_original[col].values)
         ]
 
         if col == "Target indoor temperature for VCS":
@@ -125,45 +87,15 @@ def test_snapshot_building_simulation(snapshot):
 
 
 def test_sanapshot_Apartment_building_Vent_mode_over_year(snapshot):
-    thermophys_prop = ThermostaticalProperties(
-        external_wall_area=9.6 + 16.2 + 16.2 + 21.6,
-        floor_area=6 * 8,
-        roof_area=6 * 8,
-        external_wall_r=1.797,
-        floor_r=25.246,
-        roof_r=2.992,
-    )
-    inputs = Building(
-        bui_type="Apartment building",
-        celing_to_floor_height=2.7,
-        envelope_area=171.60,
-        floor_area=48.00,
-        fenestration_area=12.00,
-        comfort_requirements="category II",
-        max_outdoor_rel_hum_accepted=85,
-        u_value_opaque=thermophys_prop.u_value_tot,
-        u_value_fen=2.984,
-        construction_mass="medium",
-        g_value_glazing_sys=0.71,
-        shading_control_setpoint=120,
-        shading_factor=0,
-        vent_rates_mu="1/h",
-        time_control_on=0,
-        time_control_off=24,
-        ti_hsp_day_start=7,
-        ti_hsp_night_start=24,
-    )
-    climate_data = get_original_test_climate_data()
+    thermophys_prop = get_appartment_bld_thermophys_props()
+    inputs = get_appartment_building(thermophys_prop)
+    climate_data = get_appartment_bld_climate_data()
 
     df_sim = run_vct_simulation(inputs, climate_data, thermophys_prop)
-    df_sim = df_sim[744:]
-    df = get_vent_mode_over_year(df_sim)
+    df = get_vent_mode_over_year(df_sim[744:])
 
-    df_original = pd.read_csv("tests/data/results_vent_mode_over_year.csv", index_col=0)
-    df.columns = ["0", "1", "2", "3"]
-    df_original.columns = ["0", "1", "2", "3"]
-
-    if not pd.concat([df, df_original]).drop_duplicates(keep=False).empty:
+    df_original = pd.read_csv("tests/inputs/data/results_vent_mode_over_year.csv", index_col=0)
+    if not compare_dataframes(df, df_original):
         raise AssertionError()
 
     result = df.to_json()
@@ -171,92 +103,34 @@ def test_sanapshot_Apartment_building_Vent_mode_over_year(snapshot):
 
 
 def test_snapshot_Apartment_building_Freq_air_change_rate(snapshot):
-    thermophys_prop = ThermostaticalProperties(
-        external_wall_area=9.6 + 16.2 + 16.2 + 21.6,
-        floor_area=6 * 8,
-        roof_area=6 * 8,
-        external_wall_r=1.797,
-        floor_r=25.246,
-        roof_r=2.992,
-    )
-    inputs = Building(
-        bui_type="Apartment building",
-        celing_to_floor_height=2.7,
-        envelope_area=171.60,
-        floor_area=48.00,
-        fenestration_area=12.00,
-        comfort_requirements="category II",
-        max_outdoor_rel_hum_accepted=85,
-        u_value_opaque=thermophys_prop.u_value_tot,
-        u_value_fen=2.984,
-        construction_mass="medium",
-        g_value_glazing_sys=0.71,
-        shading_control_setpoint=120,
-        shading_factor=0,
-        vent_rates_mu="1/h",
-        time_control_on=0,
-        time_control_off=24,
-        ti_hsp_day_start=7,
-        ti_hsp_night_start=24,
-    )
-    climate_data = get_original_test_climate_data()
+    thermophys_prop = get_appartment_bld_thermophys_props()
+    inputs = get_appartment_building(thermophys_prop)
+    climate_data = get_appartment_bld_climate_data()
 
     df_sim = run_vct_simulation(inputs, climate_data, thermophys_prop)
-    df_sim = df_sim[744:]
-    df = get_requirend_frequency_air_change_rate(df_sim, inputs)
+    df = get_requirend_frequency_air_change_rate(df_sim[744:], inputs)
 
     df_original = pd.read_csv(
-        "tests/data/results_req_freq_air_change_rate.csv", index_col=0
+        "tests/inputs/data/results_req_freq_air_change_rate.csv", index_col=0
     )
-    error = 10**-15
-    for col in df.columns:
-        if max(df[col].values - df_original[col].values) > error:
-            raise AssertionError()
+    if not compare_dataframes(df, df_original):
+        raise AssertionError()
 
     result = df.to_json()
     snapshot.assert_match(result, "req_freq_air_change_rate.json")
 
 
 def test_snapshot_Apartment_building_Annual_data(snapshot):
-    thermophys_prop = ThermostaticalProperties(
-        external_wall_area=9.6 + 16.2 + 16.2 + 21.6,
-        floor_area=6 * 8,
-        roof_area=6 * 8,
-        external_wall_r=1.797,
-        floor_r=25.246,
-        roof_r=2.992,
-    )
-    inputs = Building(
-        bui_type="Apartment building",
-        celing_to_floor_height=2.7,
-        envelope_area=171.60,
-        floor_area=48.00,
-        fenestration_area=12.00,
-        comfort_requirements="category II",
-        max_outdoor_rel_hum_accepted=85,
-        u_value_opaque=thermophys_prop.u_value_tot,
-        u_value_fen=2.984,
-        construction_mass="medium",
-        g_value_glazing_sys=0.71,
-        shading_control_setpoint=120,
-        shading_factor=0,
-        vent_rates_mu="1/h",
-        time_control_on=0,
-        time_control_off=24,
-        ti_hsp_day_start=7,
-        ti_hsp_night_start=24,
-    )
-    climate_data = get_original_test_climate_data()
+    thermophys_prop = get_appartment_bld_thermophys_props()
+    inputs = get_appartment_building(thermophys_prop)
+    climate_data = get_appartment_bld_climate_data()
 
     df_sim = run_vct_simulation(inputs, climate_data, thermophys_prop)
-    df_sim = df_sim[744:]
-    df = get_annual_data(df_sim)
+    df = get_annual_data(df_sim[744:])
 
-    df_original = pd.read_csv("tests/data/results_annual_data.csv", index_col=0)
-    error = 10**-12
-    for col in df.columns:
-        if max(df[col].values - df_original[col].values) > error:
-            raise AssertionError()
+    df_original = pd.read_csv("tests/inputs/data/results_annual_data.csv", index_col=0)
+    if not compare_dataframes(df, df_original):
+        raise AssertionError()
 
     result = df.to_json()
     snapshot.assert_match(result, "annual_data.json")
@@ -264,6 +138,47 @@ def test_snapshot_Apartment_building_Annual_data(snapshot):
 
 ####################################################################################
 
+def run_test_building_example(filename):
+    (inputs, 
+     climate_data, 
+     df_orig_sim, 
+     df_orig_vent_mode, 
+     df_orig_freq_air_change, 
+     df_orig_annual_data) = load_data_from_VCdesign(filename)
 
-# =$file.Y874*(1-$C$22)*$VCP.$C$20*$VCP.$C$10/$VCP.$C$9
-# =$'FILE'.K129*(1-$C$22)*$VCP.$C$20*$VCP.$C$10/$VCP.$C$9
+    # Run simulation
+    df = run_vct_simulation(inputs, climate_data)
+    df_vent_mode = get_vent_mode_over_year(df[744:])
+    df_freq_air_change = get_requirend_frequency_air_change_rate(df[744:], inputs)
+    df_annual_data = get_annual_data(df[744:])
+
+    # Compare Main simulation results
+    df_orig_sim = df_orig_sim.drop(columns=["D", "F", "G", "H", "AK", "AL"]) # drop Date, Month, Time, 'Same in other unit' columns
+    df = df.drop(columns=["Date", "Time", "Day"])
+
+    if not compare_dataframes(df, df_orig_sim): 
+        raise AssertionError()
+
+    # Compare Vent mode over year
+    if not compare_dataframes(df_vent_mode, df_orig_vent_mode):
+        raise AssertionError()
+
+    # Compare Frequency air change rate
+    df_orig_freq_air_change = df_orig_freq_air_change.drop(columns=['BA']) # drop extra column
+    if not compare_dataframes(df_freq_air_change, df_orig_freq_air_change):
+        raise AssertionError()
+
+    # Compare sensible energy needs
+    df_orig_annual_data = df_orig_annual_data.drop(columns=['BF', 'BH']) # drop extra columns
+    if not compare_dataframes(df_annual_data, df_orig_annual_data):
+        raise AssertionError()
+    
+
+# def test_Simulation_VCdesign_Example21():
+#     filename = "tests/inputs/data/VCdesign_Example21.xlsm"
+#     run_test_building_example(filename)
+
+
+# def test_Simulation_VCdesign_Example31():
+#     filename = "tests/inputs/data/VCdesign_Example31.xlsm"
+#     run_test_building_example(filename)

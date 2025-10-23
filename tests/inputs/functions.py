be344fe5e4c0176d9dbcf8d78b05008c883d03eb
Olga Somova <osomova@eurac.edu>
2025-10-23 16:17:16 +0200

    refactoring in test calculation

--- tests/inputs/functions.py
+++ tests/inputs/functions.py
@@ -23,44 +23,8 @@ def get_appartment_bld_climate_data() -> ClimateData:
     return climate_data
 
 
-def get_appartment_bld_thermophys_props() -> ThermostaticalProperties:
-    thermophys_prop = ThermostaticalProperties(
-        external_wall_area=9.6 + 16.2 + 16.2 + 21.6,
-        floor_area=6 * 8,
-        roof_area=6 * 8,
-        external_wall_r=1.797,
-        floor_r=25.246,
-        roof_r=2.992,
-    )
-    return thermophys_prop
 
-
-def get_appartment_building(thermophys_prop: ThermostaticalProperties) -> Building:
-    building = Building(
-        bui_type="Apartment building",
-        celing_to_floor_height=2.7,
-        envelope_area=171.60,
-        floor_area=48.00,
-        fenestration_area=12.00,
-        comfort_requirements="category II",
-        max_outdoor_rel_hum_accepted=85,
-        u_value_opaque=thermophys_prop.u_value_tot,
-        u_value_fen=2.984,
-        construction_mass="medium",
-        g_value_glazing_sys=0.71,
-        shading_control_setpoint=120,
-        shading_factor=0,
-        time_control_on=0,
-        time_control_off=24,
-        ti_day_start=7,
-        ti_night_start=24,
-        select_internal_gains='basecase',
-        select_vent_rates_calc=SELECT_VENT_RATES_CALC[0]
-    )
-    return building
-
-
-def load_data_from_VCdesign(filename):
+def load_climate_data_from_VCdesign(filename):
 
     def load_workbook_range(range_string, ws):
         col_start, col_end = re.findall("[A-Z]+", range_string)
@@ -85,33 +49,4 @@ def load_data_from_VCdesign(filename):
         df_isol_tot=isol_tot,
     )
 
-    sh = wb["VCP"]
-    inputs = Building(
-        bui_type=sh["C6"].value,
-        celing_to_floor_height=sh["C7"].value,
-        envelope_area=sh["C8"].value,
-        floor_area=sh["C9"].value,
-        fenestration_area=sh["C10"].value,
-        comfort_requirements=sh["C12"].value,
-        max_outdoor_rel_hum_accepted=sh["C13"].value,
-        u_value_opaque=sh["C16"].value,
-        u_value_fen=sh["C17"].value,
-        construction_mass=sh["C19"].value,
-        g_value_glazing_sys=sh["C20"].value,
-        shading_control_setpoint=sh["C21"].value,
-        shading_factor=sh["C22"].value,
-        # vent_rates_mu=sh["D24"].value,
-        time_control_on=sh["C32"].value,
-        time_control_off=sh["C33"].value,
-        ti_day_start=7,
-        ti_night_start=24,
-        select_vent_rates_calc=SELECT_VENT_RATES_CALC[0]
-    )
-
-    # load Results
-    df_orig_sim = load_workbook_range('D41:AT9544', sh)
-    df_orig_vent_mode = load_workbook_range('AT6:AW18', sh)
-    df_orig_freq_air_change = load_workbook_range('AY6:BB15', sh)
-    df_orig_annual_data = load_workbook_range('BE6:BK17', sh)
-
-    return inputs, climate_data, df_orig_sim, df_orig_vent_mode, df_orig_freq_air_change, df_orig_annual_data
+    return climate_data
--- tests/test_calculation.py
+++ tests/test_calculation.py
@@ -7,8 +7,6 @@ import json
 
 from venticoolpy.calculation import (
     run_vct_simulation,
-    get_climate_data_from_epw,
-    get_climate_data_from_csv,
     get_vent_mode_over_year,
     get_requirend_frequency_air_change_rate,
     get_annual_data,
@@ -18,23 +16,8 @@ from venticoolpy.new_irradiation_SFA_Perez_newCalc import get_climate_data_w_ver
 
 from inputs.functions import (
     get_appartment_bld_climate_data, 
-    get_appartment_bld_thermophys_props, 
-    get_appartment_building, 
-    load_data_from_VCdesign
+    load_climate_data_from_VCdesign
 )
-
-
-
-ERROR = 10**-8  # Acceptable error = 1e-8
-
-
-def compare_dataframes(df1: pd.DataFrame, df2: pd.DataFrame):
-    """ Compare two dataframes with tolerance 1e-8 """
-    arr1 = df1.values.astype(float)
-    arr2 = df2.values.astype(float)
-
-    are_equal = np.allclose(np.sort(arr1.flatten()), np.sort(arr2.flatten()), atol=ERROR, equal_nan=True)
-    return are_equal
 
 
 
@@ -84,13 +67,35 @@ def test_snapshot_building_simulation(snapshot):
 
 ####################################################################################
 
-def run_test_building_example(filename, snapshot):
-    (inputs, 
-     climate_data, 
-     _, 
-     _, 
-     _, 
-     _) = load_data_from_VCdesign(filename)
+
+def test_Simulation_VCdesign_Example21(snapshot):
+
+    inputs = Building(**{
+        "bui_type": "Apartment building",
+        "celing_to_floor_height": 2.6,
+        "comfort_requirements": "category II",
+        "construction_mass": "medium",
+        "envelope_area": 7.8,
+        "fenestration_area": 2,
+        "floor_area": 14,
+        "g_value_glazing_sys": 0.7,
+        "max_outdoor_rel_hum_accepted": 85,
+        "select_internal_gains": "of_bui_type",
+        "select_vent_rates_calc": "constant",
+        "shading_control_setpoint": 120,
+        "shading_factor": 0,
+        "ti_csb": 50,
+        "ti_day_start": 7,
+        "ti_hsb": 15,
+        "ti_night_start": 24,
+        "time_control_off": 24,
+        "time_control_on": 0,
+        "u_value_fen": 1.2,
+        "u_value_opaque": 0.21
+    })
+
+    filename = "tests/inputs/data/VCdesign_Example21.xlsm"
+    climate_data = load_climate_data_from_VCdesign(filename)
 
     building_str = json.dumps(inputs.__dict__, indent=4, sort_keys=True)
     snapshot.assert_match(building_str, "building.json")
@@ -111,70 +116,248 @@ def run_test_building_example(filename, snapshot):
     snapshot.assert_match(csv_df_freq_air_chg, "req_freq_air_change_rate.csv")
     snapshot.assert_match(csv_df_year, "annual_data.csv")
 
-    
-
-def test_Simulation_VCdesign_Example21(snapshot):
-    filename = "tests/inputs/data/VCdesign_Example21.xlsm"
-    run_test_building_example(filename, snapshot)
 
 
 def test_Simulation_VCdesign_Example31(snapshot):
+    inputs = Building(**{
+        "bui_type": "School",
+        "celing_to_floor_height": 3,
+        "comfort_requirements": "category II",
+        "construction_mass": "light",
+        "envelope_area": 30,
+        "fenestration_area": 30,
+        "floor_area": 50,
+        "g_value_glazing_sys": 0.7,
+        "max_outdoor_rel_hum_accepted": 85,
+        "select_internal_gains": "of_bui_type",
+        "select_vent_rates_calc": "constant",
+        "shading_control_setpoint": 120,
+        "shading_factor": 0.3,
+        "ti_csb": 50,
+        "ti_day_start": 7,
+        "ti_hsb": 15,
+        "ti_night_start": 24,
+        "time_control_off": 16,
+        "time_control_on": 8,
+        "u_value_fen": 3.5,
+        "u_value_opaque": 3
+    })
+
     filename = "tests/inputs/data/VCdesign_Example31.xlsm"
-    run_test_building_example(filename, snapshot)
+    climate_data= load_climate_data_from_VCdesign(filename)
+
+    building_str = json.dumps(inputs.__dict__, indent=4, sort_keys=True)
+    snapshot.assert_match(building_str, "building.json")
+
+    # Run simulation
+    df = run_vct_simulation(inputs, climate_data)
+    df_vent_mode = get_vent_mode_over_year(df[744:])
+    df_freq_air_change = get_requirend_frequency_air_change_rate(df[744:], inputs)
+    df_annual_data = get_annual_data(df[744:])
+
+    csv_df_december = df.tail(744).to_csv(lineterminator="\n")
+    csv_df_vent_mode = df_vent_mode.to_csv(lineterminator="\n")
+    csv_df_freq_air_chg = df_freq_air_change.to_csv(lineterminator="\n")
+    csv_df_year = df_annual_data.to_csv(lineterminator="\n")
+
+    snapshot.assert_match(csv_df_december, "simulation_december.csv")
+    snapshot.assert_match(csv_df_vent_mode, "vent_mode_over_year.csv")
+    snapshot.assert_match(csv_df_freq_air_chg, "req_freq_air_change_rate.csv")
+    snapshot.assert_match(csv_df_year, "annual_data.csv")
+
 
 
 def test_Simulation_VCdesign_Example41(snapshot):
+    inputs = Building(**{
+        "bui_type": "Apartment building",
+        "celing_to_floor_height": 2.6,
+        "comfort_requirements": "category II",
+        "construction_mass": "medium",
+        "envelope_area": 7.8,
+        "fenestration_area": 1.5,
+        "floor_area": 14,
+        "g_value_glazing_sys": 0.7,
+        "max_outdoor_rel_hum_accepted": 85,
+        "select_internal_gains": "of_bui_type",
+        "select_vent_rates_calc": "constant",
+        "shading_control_setpoint": 120,
+        "shading_factor": 0.5,
+        "ti_csb": 50,
+        "ti_day_start": 7,
+        "ti_hsb": 15,
+        "ti_night_start": 24,
+        "time_control_off": 16,
+        "time_control_on": 8,
+        "u_value_fen": 1.2,
+        "u_value_opaque": 0.24
+    })
+
     filename = "tests/inputs/data/VCdesign_Example41.xlsm"
-    run_test_building_example(filename, snapshot)
+    climate_data = load_climate_data_from_VCdesign(filename)
+
+    building_str = json.dumps(inputs.__dict__, indent=4, sort_keys=True)
+    snapshot.assert_match(building_str, "building.json")
+
+    # Run simulation
+    df = run_vct_simulation(inputs, climate_data)
+    df_vent_mode = get_vent_mode_over_year(df[744:])
+    df_freq_air_change = get_requirend_frequency_air_change_rate(df[744:], inputs)
+    df_annual_data = get_annual_data(df[744:])
+
+    csv_df_december = df.tail(744).to_csv(lineterminator="\n")
+    csv_df_vent_mode = df_vent_mode.to_csv(lineterminator="\n")
+    csv_df_freq_air_chg = df_freq_air_change.to_csv(lineterminator="\n")
+    csv_df_year = df_annual_data.to_csv(lineterminator="\n")
+
+    snapshot.assert_match(csv_df_december, "simulation_december.csv")
+    snapshot.assert_match(csv_df_vent_mode, "vent_mode_over_year.csv")
+    snapshot.assert_match(csv_df_freq_air_chg, "req_freq_air_change_rate.csv")
+    snapshot.assert_match(csv_df_year, "annual_data.csv")
+
 
 
 def test_Simulation_VCdesign_Example51(snapshot):
+    inputs = Building(**{
+        "bui_type": "Apartment building",
+        "celing_to_floor_height": 2.6,
+        "comfort_requirements": "category II",
+        "construction_mass": "medium",
+        "envelope_area": 7.8,
+        "fenestration_area": 1.5,
+        "floor_area": 14,
+        "g_value_glazing_sys": 0.7,
+        "max_outdoor_rel_hum_accepted": 85,
+        "select_internal_gains": "of_bui_type",
+        "select_vent_rates_calc": "constant",
+        "shading_control_setpoint": 120,
+        "shading_factor": 0.5,
+        "ti_csb": 50,
+        "ti_day_start": 7,
+        "ti_hsb": 15,
+        "ti_night_start": 24,
+        "time_control_off": 16,
+        "time_control_on": 8,
+        "u_value_fen": 1.2,
+        "u_value_opaque": 0.24
+    })
+
     filename = "tests/inputs/data/VCdesign_Example51.xlsm"
-    run_test_building_example(filename, snapshot)
+    climate_data = load_climate_data_from_VCdesign(filename)
+
+    building_str = json.dumps(inputs.__dict__, indent=4, sort_keys=True)
+    snapshot.assert_match(building_str, "building.json")
+
+    # Run simulation
+    df = run_vct_simulation(inputs, climate_data)
+    df_vent_mode = get_vent_mode_over_year(df[744:])
+    df_freq_air_change = get_requirend_frequency_air_change_rate(df[744:], inputs)
+    df_annual_data = get_annual_data(df[744:])
+
+    csv_df_december = df.tail(744).to_csv(lineterminator="\n")
+    csv_df_vent_mode = df_vent_mode.to_csv(lineterminator="\n")
+    csv_df_freq_air_chg = df_freq_air_change.to_csv(lineterminator="\n")
+    csv_df_year = df_annual_data.to_csv(lineterminator="\n")
+
+    snapshot.assert_match(csv_df_december, "simulation_december.csv")
+    snapshot.assert_match(csv_df_vent_mode, "vent_mode_over_year.csv")
+    snapshot.assert_match(csv_df_freq_air_chg, "req_freq_air_change_rate.csv")
+    snapshot.assert_match(csv_df_year, "annual_data.csv")
+
 
 
 def test_Simulation_VCdesign_Example61(snapshot):
+    inputs = Building(**{
+        "bui_type": "Hotel",
+        "celing_to_floor_height": 2.6,
+        "comfort_requirements": "category I",
+        "construction_mass": "light",
+        "envelope_area": 7.8,
+        "fenestration_area": 1.5,
+        "floor_area": 14,
+        "g_value_glazing_sys": 0.7,
+        "max_outdoor_rel_hum_accepted": 85,
+        "select_internal_gains": "of_bui_type",
+        "select_vent_rates_calc": "constant",
+        "shading_control_setpoint": 120,
+        "shading_factor": 0,
+        "ti_csb": 50,
+        "ti_day_start": 7,
+        "ti_hsb": 15,
+        "ti_night_start": 24,
+        "time_control_off": 16,
+        "time_control_on": 8,
+        "u_value_fen": 1.7,
+        "u_value_opaque": 2.5
+    })
+
     filename = "tests/inputs/data/VCdesign_Example61.xlsm"
-    run_test_building_example(filename, snapshot)
+    climate_data = load_climate_data_from_VCdesign(filename)
+
+    building_str = json.dumps(inputs.__dict__, indent=4, sort_keys=True)
+    snapshot.assert_match(building_str, "building.json")
+
+    # Run simulation
+    df = run_vct_simulation(inputs, climate_data)
+    df_vent_mode = get_vent_mode_over_year(df[744:])
+    df_freq_air_change = get_requirend_frequency_air_change_rate(df[744:], inputs)
+    df_annual_data = get_annual_data(df[744:])
+
+    csv_df_december = df.tail(744).to_csv(lineterminator="\n")
+    csv_df_vent_mode = df_vent_mode.to_csv(lineterminator="\n")
+    csv_df_freq_air_chg = df_freq_air_change.to_csv(lineterminator="\n")
+    csv_df_year = df_annual_data.to_csv(lineterminator="\n")
+
+    snapshot.assert_match(csv_df_december, "simulation_december.csv")
+    snapshot.assert_match(csv_df_vent_mode, "vent_mode_over_year.csv")
+    snapshot.assert_match(csv_df_freq_air_chg, "req_freq_air_change_rate.csv")
+    snapshot.assert_match(csv_df_year, "annual_data.csv")
+
 
 
 def test_Simulation_VCdesign_Example62(snapshot):
+    inputs = Building(**{
+        "bui_type": "Office",
+        "celing_to_floor_height": 3,
+        "comfort_requirements": "category I",
+        "construction_mass": "medium",
+        "envelope_area": 50,
+        "fenestration_area": 25,
+        "floor_area": 50,
+        "g_value_glazing_sys": 0.7,
+        "max_outdoor_rel_hum_accepted": 85,
+        "select_internal_gains": "of_bui_type",
+        "select_vent_rates_calc": "constant",
+        "shading_control_setpoint": 120,
+        "shading_factor": 0,
+        "ti_csb": 50,
+        "ti_day_start": 7,
+        "ti_hsb": 15,
+        "ti_night_start": 24,
+        "time_control_off": 16,
+        "time_control_on": 8,
+        "u_value_fen": 1.7,
+        "u_value_opaque": 2.5
+    })
+
     filename = "tests/inputs/data/VCdesign_Example62.xlsm"
-    run_test_building_example(filename, snapshot)
+    climate_data = load_climate_data_from_VCdesign(filename)
 
+    building_str = json.dumps(inputs.__dict__, indent=4, sort_keys=True)
+    snapshot.assert_match(building_str, "building.json")
 
-
-# new irradiation calculation:
-
-# TODO: add new unit tests
-
-def test_simulation_dummy(snapshot):
-    building = Building(
-        bui_type="Apartment building",
-        celing_to_floor_height=2.7,
-        envelope_area=171.60,
-        floor_area=48.00,
-        fenestration_area=12.00,
-        comfort_requirements="category II",
-        max_outdoor_rel_hum_accepted=85,
-        u_value_opaque=0.315822914673981,
-        u_value_fen=2.984,
-        construction_mass="medium",
-        g_value_glazing_sys=0.71,
-        shading_control_setpoint=120,
-        shading_factor=0,
-        time_control_on=0,
-        time_control_off=24,
-    )
-     
-    climate_filename = 'tests/inputs/data/climate/4A_London_TMY_2001-2020.epw'
-    climate_data = get_climate_data_w_vert_irrad_from_epw(climate_filename, "SW")
-
-    df = run_vct_simulation(building, climate_data)
+    # Run simulation
+    df = run_vct_simulation(inputs, climate_data)
     df_vent_mode = get_vent_mode_over_year(df[744:])
-    df_freq_air_change = get_requirend_frequency_air_change_rate(df[744:], building)
+    df_freq_air_change = get_requirend_frequency_air_change_rate(df[744:], inputs)
     df_annual_data = get_annual_data(df[744:])
 
-    snapshot.assert_match(df_vent_mode.to_json(), "test_simulation_dummy_df_vent_mode.yml")
-    snapshot.assert_match(df_freq_air_change.to_json(), "test_simulation_dummy_df_freq_air_change.yml")
-    snapshot.assert_match(df_annual_data.to_json(), "test_simulation_dummy_df_annual_data.yml")
+    csv_df_december = df.tail(744).to_csv(lineterminator="\n")
+    csv_df_vent_mode = df_vent_mode.to_csv(lineterminator="\n")
+    csv_df_freq_air_chg = df_freq_air_change.to_csv(lineterminator="\n")
+    csv_df_year = df_annual_data.to_csv(lineterminator="\n")
+
+    snapshot.assert_match(csv_df_december, "simulation_december.csv")
+    snapshot.assert_match(csv_df_vent_mode, "vent_mode_over_year.csv")
+    snapshot.assert_match(csv_df_freq_air_chg, "req_freq_air_change_rate.csv")
+    snapshot.assert_match(csv_df_year, "annual_data.csv")
+
